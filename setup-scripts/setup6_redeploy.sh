f99fadfe43025d958961c1211712f865949391f2
Matt Nikkel <nikkelma@vt.edu>
2017-05-03 16:45:55 -0400

    Tweak redeploy for easy restarts

--- setup-scripts/setup6_redeploy.sh
+++ setup-scripts/setup6_redeploy.sh
@@ -4,20 +4,20 @@ eval $(docker-machine env manager)
 docker stack rm finalT3
 docker volume prune
 
+git pull
 
-eval $(docker-machine env worker1)
-docker volume prune
-
-
-eval $(docker-machine env worker2)
-docker volume prune
+# eval $(docker-machine env worker1)
+# docker volume prune
 
 
-eval $(docker-machine env worker3)
-docker volume prune
+# eval $(docker-machine env worker2)
+# docker volume prune
+
+
+# eval $(docker-machine env worker3)
+# docker volume prune
 
 
 eval $(docker-machine env manager)
 
-docker stack deploy -c final-swarm.yml finalT3 && \
-    docker run --rm --network=mainnet -it musica-storage-setup
+docker stack deploy -c final-swarm.yml finalT3
