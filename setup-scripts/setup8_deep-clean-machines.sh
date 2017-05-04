e98865d0c2dbda99acadb914ccd7a255c361bf2e
Matt Nikkel <nikkelma@vt.edu>
2017-05-04 18:57:42 -0400

    Full VM clean script

---
+++ setup-scripts/setup8_deep-clean-machines.sh
@@ -0,0 +1,15 @@
+eval $(docker-machine env worker1)
+docker rmi -f $(docker images -q)
+docker volume prune -f
+
+eval $(docker-machine env worker2)
+docker rmi -f $(docker images -q)
+docker volume prune -f
+
+eval $(docker-machine env worker3)
+docker rmi -f $(docker images -q)
+docker volume prune -f
+
+eval $(docker-machine env manager)
+docker rmi -f $(docker images -q)
+docker volume prune -f
