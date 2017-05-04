b3338f93490db2201ce8a72e544a0c8243f7a438
Matt Nikkel <nikkelma@vt.edu>
2017-05-04 15:48:45 -0400

    Add clean-script, redefine swarm for quicker redeploy

--- setup-scripts/setup4_machine-builds.sh
+++ setup-scripts/setup4_machine-builds.sh
@@ -18,35 +18,35 @@ docker build -t musica-storage-webserver ./reco_storage
 
 eval $(docker-machine env worker1)
 
-docker build -t musica-fingerprint ./fingerprint
+# docker build -t musica-fingerprint ./fingerprint
 
-docker build -t musica-suggest ./music_suggest
+# docker build -t musica-suggest ./music_suggest
 
 docker build -t musica-storage-mongodb ./reco_storage/docker/reco-storage-mongodb
 
-docker build -t musica-storage-webserver ./reco_storage
+# docker build -t musica-storage-webserver ./reco_storage
 
 
 
 eval $(docker-machine env worker2)
 
-docker build -t musica-fingerprint ./fingerprint
+# docker build -t musica-fingerprint ./fingerprint
 
-docker build -t musica-suggest ./music_suggest
+# docker build -t musica-suggest ./music_suggest
 
 docker build -t musica-storage-mongodb ./reco_storage/docker/reco-storage-mongodb
 
-docker build -t musica-storage-webserver ./reco_storage
+# docker build -t musica-storage-webserver ./reco_storage
 
 
 
 eval $(docker-machine env worker3)
 
-docker build -t musica-fingerprint ./fingerprint
+# docker build -t musica-fingerprint ./fingerprint
 
-docker build -t musica-suggest ./music_suggest
+# docker build -t musica-suggest ./music_suggest
 
 docker build -t musica-storage-mongodb ./reco_storage/docker/reco-storage-mongodb
 
-docker build -t musica-storage-webserver ./reco_storage
+# docker build -t musica-storage-webserver ./reco_storage
 
---
+++ setup-scripts/setup7_clean-machines.sh
@@ -0,0 +1,14 @@
+eval $(docker-machine env manager)
+docker rmi -f $(docker images -f dangling=true -q)
+
+
+eval $(docker-machine env worker1)
+docker rmi -f $(docker images -f dangling=true -q)
+
+
+eval $(docker-machine env worker2)
+docker rmi -f $(docker images -f dangling=true -q)
+
+
+eval $(docker-machine env worker3)
+docker rmi -f $(docker images -f dangling=true -q)
