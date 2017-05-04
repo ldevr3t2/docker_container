b40c0fa1c236fa59687c727289e277f96d70c9a9
Matt Nikkel <nikkelma@vt.edu>
2017-05-04 18:54:08 -0400

    Initial fully automated storage setup

--- setup-scripts/setup5_first-deploy.sh
+++ setup-scripts/setup5_first-deploy.sh
@@ -5,4 +5,8 @@ eval $(docker-machine env manager)
 docker network create --driver=overlay --attachable mainnet
 
 docker stack deploy -c final-swarm.yml finalT3 && \
-    docker run --rm --network=mainnet -it musica-storage-setup
+    docker service update finalT3_reco-storage-router --replicas 0 && \
+    docker run --rm --network=mainnet -it musica-storage-setup ./setup-storage_1.sh && \
+    docker service update finalT3_reco-storage-router --replicas 0 && \
+    sleep 5 && \
+    docker run --rm --network=mainnet -it musica-storage-setup ./setup-storage_2.sh
